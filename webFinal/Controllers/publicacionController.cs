1c62e434a39a3c18c0b899c70e6fa55337c41629
RaulValencia04 <raulvalenciau@gmail.com>
2023-05-26 22:27:13 -0600

    publicacione funcionales *no bomitas*

---
+++ webFinal/Controllers/publicacionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webFinal.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace webFinal.Controllers
+{
+    public class publicacionController : Controller
+    {
+        private readonly empleosDBContext _empleosDBContext;
+
+        public publicacionController(empleosDBContext empleosDBContext1)
+        {
+            _empleosDBContext =  empleosDBContext1;
+        }
+
+        public ActionResult Index()
+        {
+            List<Publicacion> publi = _empleosDBContext.Publicaciones
+                .Select(p => new Publicacion
+                {
+                    IdPublicacion = p.IdPublicacion,
+                    IdEmpresa = p.IdEmpresa,
+                    Titulo = p.Titulo,
+                    Descripcion = p.Descripcion,
+                    FechaPublicacion = p.FechaPublicacion
+                })
+                .ToList();
+
+            return View(publi);
+        }
+
+    }
+
+}
