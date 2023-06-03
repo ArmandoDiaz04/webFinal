e763e7e062715ea817baa127feaf375610596db3
RaulValencia04 <raulvalenciau@gmail.com>
2023-06-03 02:40:45 -0600

    vista recursos agregadad

---
+++ webFinal/Controllers/RecursosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webFinal.Controllers
+{
+    public class RecursosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
