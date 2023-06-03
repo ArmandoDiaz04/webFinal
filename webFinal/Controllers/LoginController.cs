ad1b6a0501598a6a86a3069fc6dff809f9d3b615
Juan92A <acunajuan429@gmail.com>
2023-06-03 07:32:24 -0600

    merge kv2

--- webFinal/Controllers/LoginController.cs
+++ webFinal/Controllers/LoginController.cs
@@ -18,6 +18,7 @@
             public IActionResult Index(string Email, string Password)
             {
                 var user = _dbContext.Usuarios.FirstOrDefault(u => u.Email == Email && u.contrasenia == Password);
+                var empresa = _dbContext.Empresas.FirstOrDefault(u => u.Email == Email && u.contrasenia == Password);
 
                 if (user != null)
                 {
@@ -25,12 +26,32 @@
                     HttpContext.Session.SetString("UserId", user.IdUsuario.ToString());
                     HttpContext.Session.SetString("RolId", user.idrol.ToString());
 
-                // Redirigir a la página de inicio después de iniciar sesión
-                return RedirectToAction("Index", "Home");
-                }
 
-                // Usuario no válido, muestra un mensaje de error
-                ViewData["Error"] = "Credenciales inválidas";
+
+                    // Redirigir a la página de inicio después de iniciar sesión
+                    return RedirectToAction("Index", "Home");
+                }
+          
+
+                if (empresa != null)
+                {
+                    // Guardar estado de inicio de sesión en una variable de sesión
+                    HttpContext.Session.SetString("UserId", empresa.IdEmpresa.ToString());
+                    HttpContext.Session.SetString("RolId", empresa.idrol.ToString());
+
+
+
+                    // Redirigir a la página de inicio después de iniciar sesión
+                    return RedirectToAction("Index", "Home");
+                }
+         
+           
+
+
+
+
+            // Usuario no válido, muestra un mensaje de error
+            ViewData["Error"] = "Credenciales inválidas";
                 return View();
             }
         public IActionResult Logout()
