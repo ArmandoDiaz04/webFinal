fe3f75948b9423075b74fde85e75bb64f2f4d088
Juan92A <acunajuan429@gmail.com>
2023-06-09 21:17:20 -0600

    prueubas

--- webFinal/Controllers/ValoracionEmpresasController.cs
+++ webFinal/Controllers/ValoracionEmpresasController.cs
@@ -88,17 +88,21 @@ namespace webFinal.Controllers
         // POST: ValoracionEmpresas/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+    
         [ValidateAntiForgeryToken]
+        [HttpPost]
         public async Task<IActionResult> Guardar(ValoracionEmpresa valoracionEmpresa)
         {
-           
+            if (ModelState.IsValid)
+            {
                 _context.Add(valoracionEmpresa);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("Index", "ValoracionEmpresas");           
+                return RedirectToAction("Index", "ValoracionEmpresas");
+            }
 
-          
+            return View(valoracionEmpresa);
         }
+
 
 
         // GET: ValoracionEmpresas/Edit/5
